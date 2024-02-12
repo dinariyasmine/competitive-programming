ddd49e39ad8895ac96219894283a1e27b5b65867
Yasmine Dinari <ly_dinari@esi.dz>
2024-02-12 20:14:48 +0100

    Update Dictionaries&Maps.py

--- Dictionaries&Maps.py
+++ Dictionaries&Maps.py
@@ -2,10 +2,10 @@
 # Read the number of entries in the phone book
 n = int(input())
 
-# Create an empty phone book dictionary
+
 phone_book = {}
 
-# populate the dictionary
+
 for _ in range(n):
     name, number = input().split()
     phone_book[name] = number
@@ -13,10 +13,10 @@ for _ in range(n):
 
 while True:
     try:
-        # Read query
+        
         query = input().strip()
         
-        # Check if exists in the dictionary
+        
         if query in phone_book:
             print(f"{query}={phone_book[query]}")
         else:
