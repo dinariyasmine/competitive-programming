9c57302cd8b77d8d0659bace542779efb7033fb0
Yesmean <ly_dinari@esi.dz>
2024-02-12 21:06:28 +0100

    challenges

--- Largest Perimeter Triangle.py
+++ Largest Perimeter Triangle.py
@@ -6,11 +6,8 @@ class Solution:
         return a + b > c and a + c > b and b + c > a
     
     def largestPerimeter(self, nums: List[int]) -> int:
-        maxi = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                for k in range(j + 1, len(nums)):
-                    if self.can_form_triangle(nums[i], nums[j], nums[k]):
-                        perimeter = nums[i] + nums[j] + nums[k]
-                        maxi = max(maxi , perimeter)
-        return maxi
+        nums.sort(reverse=True) 
+        for i in range(len(nums) - 2):
+            if self.can_form_triangle(nums[i], nums[i + 1], nums[i + 2]):
+                return nums[i] + nums[i + 1] + nums[i + 2]
+        return 0 
