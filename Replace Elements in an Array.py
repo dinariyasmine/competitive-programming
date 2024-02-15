3ea42e3493aaa40a4a972ceade53032abf150252
Yasmine Dinari <ly_dinari@esi.dz>
2024-02-15 20:04:06 +0100

    Update Replace Elements in an Array.py

--- Replace Elements in an Array.py
+++ Replace Elements in an Array.py
@@ -1,8 +1,13 @@
 class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
 
+        index_map = {num: i for i, num in enumerate(nums)}
+        
         for operation in operations:
-                index = nums.index(operation[0])
-                nums[index] = operation[1]
+            if operation[0] in index_map:
+                old_num_index = index_map[operation[0]]
+                nums[old_num_index] = operation[1]
+                index_map.pop(operation[0])
+                index_map[operation[1]] = old_num_index
         return nums
         
