c7ec373eb176a3a08927248915751214340c8eb6
Yasmine Dinari <ly_dinari@esi.dz>
2024-02-16 20:04:52 +0100

    Create Frequency Tracker.py

---
+++ Frequency Tracker.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+class FrequencyTracker(object):
+    def __init__(self) -> None:
+        self.num = Counter()
+        self.freq= Counter()
+        
+    def add(self, number):
+        self.freq[self.num[number]] -= 1
+        self.num[number] += 1
+        self.freq[self.num[number]] += 1
+        
+    def deleteOne(self, number):
+        if self.num[number]:
+            self.freq[self.num[number]] -= 1
+            self.num[number] -= 1
+            if self.num[number] == 0: 
+                del self.num[number]
+            self.freq[self.num[number]] += 1
+
+    def hasFrequency(self, frequency):
+        return self.freq[frequency] > 0
+
