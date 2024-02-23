b5a8498d59367a747c831311f643476e2d6b1e73
Yasmine Dinari <ly_dinari@esi.dz>
2024-02-23 18:38:47 +0100

    Create Apply Operations to an Array.py

---
+++ Apply Operations to an Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for i in range(n - 1):
+            if nums[i] == nums[i + 1]:
+                nums[i] *= 2
+                nums[i + 1] = 0
+        zeros = nums.count(0)
+        non_zeros = [num for num in nums if num != 0]
+        return non_zeros + [0] * zeros
